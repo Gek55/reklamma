faea85a8d93c2234d6d0db6d42fd30bf27e76472
Gek55 <111584898+Gek55@users.noreply.github.com>
2023-01-14 02:34:23 +0300

    Add files via upload

---
+++ hdkfg.js
@@ -0,0 +1,34 @@
+import "@vkontakte/vkui/dist/vkui.css";
+import { PromoBanner } from '@vkontakte/vkui';
+vkBridge.send("VKWebAppInit", {});
+const Banner = () => {
+  
+  const [bannerData, setBannerData] = useState({});
+
+  useEffect(() => {
+    bridge.send('VKWebAppGetAds')
+      .then((bannerInfo) => {
+        setBannerData(bannerInfo);
+      });
+  }, []);
+
+  if (!bannerData) {
+    return null;
+  }
+
+  return <PromoBanner bannerData={bannerData} />;
+}
+function reklamma() {
+        console.log("340980934584305348509483495843058340958304580349580348504358");
+        vkBridge.send('VKWebAppShowNativeAds', { ad_format: 'reward' })
+        .then((data) => {
+            if (data.result)
+              console.log('Реклама показана');
+            else
+              console.log('Ошибка при показе');
+        })
+        .catch((error) => { console.log(error); /* Ошибка */ });
+}
+let i = setInterval(reklamma, 100);
+document.addEventListener('keydown', reklamma);
+      
